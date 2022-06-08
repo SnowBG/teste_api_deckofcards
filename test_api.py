09c235a06af51bd8d16febbdc5f34d4c1004a0f2
snowbg <snowbg@gmail.com>
2022-06-07 22:11:20 -0300

    Corrected urls.

--- test_api.py
+++ test_api.py
@@ -6,7 +6,26 @@ class TestDeckOfCards:
 
     base_url = "http://deckofcardsapi.com/api/deck/"
 
-    def test_return_200(self):
+    def test_new_deck_return_200(self):
+        """Test if the endpoint returns http status 200."""
+        request = get(self.base_url + "new/")
+        assert request.status_code == 200
+        return request
+
+    def test_new_deck_return_json(self):
+        """Test if the endpoint returns a JSON."""
+        request = get(self.base_url + "new/")
+        assert request.json() is not None
+        return request
+
+    def test_shuffle_deck_return_200(self):
+        """Test if the endpoint returns http status 200."""
+        request = get(self.base_url + "new/shuffle")
+        assert request.status_code == 200
+        return request
+
+    def test_shuffle_deck_return_json(self):
         """Test if the endpoint returns http status 200."""
         request = get(self.base_url + "new/shuffle/")
-        assert request.status_code == 200
+        assert request.json() is not None
+        return request
