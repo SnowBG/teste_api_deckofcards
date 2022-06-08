e36f78074807cf74bcb1602c5cce31cb99305840
snowbg <snowbg@gmail.com>
2022-06-07 21:08:44 -0300

    Created test for shuffle return.

--- test_api.py
+++ test_api.py
@@ -4,10 +4,9 @@ from httpx import get
 class TestDeckOfCards:
     """Set of tests for Deck of Cards API."""
 
-    def __init__(self):
-        base_url = "http://deckofcardsapi.com/api/deck/"
+    base_url = "http://deckofcardsapi.com/api/deck/"
 
     def test_return_200(self):
         """Test if the endpoint returns http status 200."""
-        request = get(self.base_url)
+        request = get(self.base_url + "new/shuffle/")
         assert request.status_code == 200
